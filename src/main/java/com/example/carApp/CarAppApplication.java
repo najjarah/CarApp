19961f269fc598ab6411d11d653c6b08d98c654b
ahmed <ahmed@LAPTOP-DTIN7NTV>
2018-12-17 20:25:26 +0200

    my second push

--- src/main/java/com/example/carApp/CarAppApplication.java
+++ src/main/java/com/example/carApp/CarAppApplication.java
@@ -2,12 +2,17 @@ package com.example.carApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @SpringBootApplication
 public class CarAppApplication {
+	private static final Logger logger = LoggerFactory.getLogger(CarAppApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarAppApplication.class, args);
+		logger.info("Hello Spring Boot");
 	}
 
 }
